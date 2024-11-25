9c36dcba3fd9568897faeb28e7c20d6ac8e1740a
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2024-11-25 03:20:42 +0000

    List<T>

---
+++ target/debug/deps/too_many_linked_lists-aacc3a1908073122.d
@@ -0,0 +1,9 @@
+/workspaces/too-many-linked-lists/target/debug/deps/libtoo_many_linked_lists-aacc3a1908073122.rmeta: src/lib.rs src/first.rs src/second.rs
+
+/workspaces/too-many-linked-lists/target/debug/deps/libtoo_many_linked_lists-aacc3a1908073122.rlib: src/lib.rs src/first.rs src/second.rs
+
+/workspaces/too-many-linked-lists/target/debug/deps/too_many_linked_lists-aacc3a1908073122.d: src/lib.rs src/first.rs src/second.rs
+
+src/lib.rs:
+src/first.rs:
+src/second.rs:
---
+++ target/debug/libtoo_many_linked_lists.d
@@ -0,0 +1,1 @@
+/workspaces/too-many-linked-lists/target/debug/libtoo_many_linked_lists.rlib: /workspaces/too-many-linked-lists/src/first.rs /workspaces/too-many-linked-lists/src/lib.rs /workspaces/too-many-linked-lists/src/second.rs
